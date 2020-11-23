f13119d4f0451960fc9986d1c113320035512010
zhuwenjoyce <ZHUwen12@!>
2020-11-24 00:19:16 +0800

    update readme.MD

--- src/main/java/com/joyce/r2dbc/mysql/dao/UserR2dbcRepository.java
+++ src/main/java/com/joyce/r2dbc/mysql/dao/UserR2dbcRepository.java
@@ -21,4 +21,19 @@ public interface UserR2dbcRepository extends R2dbcRepository<UserModel, Long> {
     // 尚未实验成功
     @Query("select * from t_users where username = :username and age > :age")
     Mono<UserDto> findByUsernameAndAge(UserDto username);
+
+//    List<Person> findByEmailAddressAndLastname(EmailAddress emailAddress, String lastname);
+//
+//    // Enables the distinct flag for the query
+//    List<Person> findDistinctPeopleByLastnameOrFirstname(String lastname, String firstname);
+//    List<Person> findPeopleDistinctByLastnameOrFirstname(String lastname, String firstname);
+//
+//    // Enabling ignoring case for an individual property
+//    List<Person> findByLastnameIgnoreCase(String lastname);
+//    // Enabling ignoring case for all suitable properties
+//    List<Person> findByLastnameAndFirstnameAllIgnoreCase(String lastname, String firstname);
+//
+//    // Enabling static ORDER BY for a query
+//    List<Person> findByLastnameOrderByFirstnameAsc(String lastname);
+//    List<Person> findByLastnameOrderByFirstnameDesc(String lastname);
 }
